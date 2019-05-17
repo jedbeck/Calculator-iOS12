af852c6487f6247f0f00e23c38dfbb8eca2f0b80
Jason Becker <jbecker@newsbank.com>
2019-05-17 14:37:28 -0400

    Using computed properties to handle the interaction with the displayLabel

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,11 +14,37 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber : Bool = true
     
+    private var displayValue: Double {
+        get{
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a double")
+            }
+            return number
+        }
+        set{
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
+        
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue = displayValue * -1
+            }
+            else if calcMethod == "AC" {
+                displayValue = 0
+            }
+            else if calcMethod == "%" {
+                displayValue = displayValue / 100
+            }
+        }
+        
     }
 
     
@@ -32,6 +58,16 @@ class ViewController: UIViewController {
                 isFinishedTypingNumber = false
             }
             else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text! +=  numValue
             }
         }
